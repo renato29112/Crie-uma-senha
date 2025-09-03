9cc7a4d9bd7e244f762e3bea440af5c143c571e4
renato29112 <140640446+renato29112@users.noreply.github.com>
2025-09-03 14:05:28 -0300

    00

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 const checkbox = document.querySelectorAll('.checkbox');
 const botoes = document.querySelectorAll('.parametro-senha-botao');
-const forcaSenha = document.querySelector('forca');
-botoes[0].onclick = diminuitamanho;
-botoes[1].onclick = aumentatamanho;
+const forcaSenha = document.querySelector('.forca');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho (){
+    if (tamanhoSenha > 1){
+        tamanhoSenha;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+const campoSenha = document.querySelector('#campo-senha');
