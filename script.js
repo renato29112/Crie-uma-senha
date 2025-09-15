0b91ade2e5474ce932594713cc2af96092f8b07b
renato29112 <140640446+renato29112@users.noreply.github.com>
2025-09-15 09:28:41 -0300

    00

--- script.js
+++ script.js
@@ -52,6 +52,12 @@ function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57){
-        
+        forcaSenha.classList.add('forte');
+    }else if (entropia > 35 && entropia < 57){
+        forcaSenha.classList.add('media');
+    }else if (entropia < 35){
+        forcaSenha.classList.add('forca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/100e6*60*24)) + "dias para descobrir essa senha";
 }
